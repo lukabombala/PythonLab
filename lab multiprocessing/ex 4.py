0002f30a0486a564b2f3be496510fae8b9d5a74c
lukabombala <14.1996Impeesateam>
2021-11-04 19:26:22 +0100

    added final solutions for multiprocessing

--- lab multiprocessing/ex 4.py
+++ lab multiprocessing/ex 4.py
@@ -7,7 +7,7 @@ def f(x):
 
 if __name__ == '__main__':
     num = [int(i) for i in input("Podaj 10 liczb oddzielonych spacją: ").split(" ")]
-    with mp.Pool(10) as p:
+    with mp.Pool(5) as p:
         output = p.map(f, num)
 
     print(output)
